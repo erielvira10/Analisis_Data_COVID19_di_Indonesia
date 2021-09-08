0fd96754f5a0f8803bbcb0af57ffa9a48dded6cd
Eri Elvira <65483196+erielvira10@users.noreply.github.com>
2021-09-07 19:39:19 -0800

    add

---
+++ cov_jabar.r
@@ -0,0 +1,7 @@
+library(httr)
+resp_jabar <- GET("https://data.covid19.go.id/public/api/prov_detail_JAWA_BARAT.json")
+cov_jabar_raw <- content(resp_jabar, as = "parsed", simplifyVector = TRUE)
+names(cov_jabar_raw)
+cov_jabar_raw$kasus_total
+cov_jabar_raw$meninggal_persen
+cov_jabar_raw$sembuh_persen
