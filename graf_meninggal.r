5ffb07fa5d254c90a4cccf9a9fbc6d0de93c03ab
Eri Elvira <65483196+erielvira10@users.noreply.github.com>
2021-09-07 20:12:16 -0800

    add

---
+++ graf_meninggal.r
@@ -0,0 +1,17 @@
+library(ggplot2)
+library(hrbrthemes)
+ggplot(new_cov_jabar, aes(tanggal, meninggal)) +
+  geom_col(fill = "darkslategray4") +
+  labs(
+    x = NULL,
+    y = "Jumlah kasus",
+    title = "Kasus Harian Meninggal Akibat COVID-19 di Jawa Barat",
+    caption = "Sumber data: covid.19.go.id"
+  ) +
+  theme_ipsum(
+    base_size = 13, 
+    plot_title_size = 21,
+    grid = "Y",
+    ticks = TRUE
+  ) +
+  theme(plot.title.position = "plot")
