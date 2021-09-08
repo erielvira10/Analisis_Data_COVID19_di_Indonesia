00bf30335d4406299cfe7828fceae7b24edc5421
Eri Elvira <65483196+erielvira10@users.noreply.github.com>
2021-09-07 20:50:06 -0800

    add

---
+++ transformasi_data.r
@@ -0,0 +1,14 @@
+library(dplyr)
+library(tidyr)
+dim(cov_jabar_akumulasi)
+cov_jabar_akumulasi_pivot <- 
+  cov_jabar_akumulasi %>% 
+  gather(
+    key = "kategori",
+    value = "jumlah",
+    -tanggal
+  ) %>% 
+  mutate(
+    kategori = sub(pattern = "akumulasi_", replacement = "", kategori)
+  )
+dim(cov_jabar_akumulasi_pivot)
