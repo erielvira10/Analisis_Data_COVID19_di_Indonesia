32185c63a6c73ed54aa068d98227dab5ccb95925
Eri Elvira <65483196+erielvira10@users.noreply.github.com>
2021-09-07 20:28:01 -0800

    add

---
+++ pola_dinamika.r
@@ -0,0 +1,10 @@
+library(dplyr)
+cov_jabar_akumulasi <- 
+  new_cov_jabar %>% 
+  transmute(
+    tanggal,
+    akumulasi_aktif = cumsum(kasus_baru) - cumsum(sembuh) - cumsum(meninggal),
+    akumulasi_sembuh = cumsum(sembuh),
+    akumulasi_meninggal = cumsum(meninggal)
+  )
+tail(cov_jabar_akumulasi)
