54e4b4ce13515c19cb7cf5730bc95c1abe48839f
Eri Elvira <65483196+erielvira10@users.noreply.github.com>
2021-09-07 20:06:16 -0800

    add

---
+++ graf_sembuh.r
@@ -0,0 +1,18 @@
+library(ggplot2)
+library(hrbrthemes)
+
+ggplot(new_cov_jabar, aes(tanggal, sembuh)) +
+  geom_col(fill = "olivedrab2") +
+  labs(
+    x = NULL,
+    y = "Jumlah kasus",
+    title = "Kasus Harian Sembuh Dari COVID-19 di Jawa Barat",
+    caption = "Sumber data: covid.19.go.id"
+  ) +
+  theme_ipsum(
+    base_size = 13, 
+    plot_title_size = 21,
+    grid = "Y",
+    ticks = TRUE
+  ) +
+  theme(plot.title.position = "plot")
